aa7a59003b0337f050443c6cb7727ae0e5d7ba28
Wyatt92 <vi_hoang@hotmail.de>
2017-08-07 22:38:52 +0200

    #1 Introduction & the Tile Class

--- src/Tile.java
+++ src/Tile.java
@@ -1,11 +1,15 @@
 /**
  * Created by Viet on 5/22/2017.
  */
-public class Tile {
+public abstract class Tile {
 
     int tileCoordinate;
 
     Tile(int tileCoordinate) {
-        this.tileCoordinate
+        this.tileCoordinate = tileCoordinate;
     }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
 }
