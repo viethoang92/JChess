d3122deb8eeb6eaf66672f8ad1d06650ab142d5f
Wyatt92 <vi_hoang@hotmail.de>
2017-08-08 01:04:24 +0200

    #2 createTile Tile and Collections.UnmodifiableMap

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -3,6 +3,7 @@ package com.chess.engine.board;
 
 import com.chess.engine.pieces.Piece;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,14 +11,20 @@ public abstract class Tile {
 
     protected final int tileCoordinate;
 
-    private static final Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
         final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
 
         for(int i=0; i<64; i++) {
             emptyTileMap.put(i, new EmptyTile(i));
         }
 
-        return emptyTileMap;
+        return Collections.unmodifiableMap(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
     }
 
     Tile(int tileCoordinate) {
