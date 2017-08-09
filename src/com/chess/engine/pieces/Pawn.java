dc546e98f8af9cc6307352dfd96c9a4b3278db3f
Wyatt92 <vi_hoang@hotmail.de>
2017-08-09 02:09:18 +0200

    #11 Pawn Cleanup

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -13,18 +13,19 @@ public class Pawn extends Piece{
 
     private final static int[] CANDIDATE_MOVE_COORDINATE = {8};
 
-    Pawn(int piecePosition, Alliance pieceAlliance) {
+    //Constructor
+    Pawn(final int piecePosition, final Alliance pieceAlliance) {
         super(piecePosition, pieceAlliance);
     }
 
     @Override
-    public Collection<Move> calculateLegalMoves(Board board) {
+    public Collection<Move> calculateLegalMoves(final Board board) {
 
         final List<Move> legalMoves = new ArrayList<>();
 
         for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE) {
 
-            int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+            final int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
 
             if(!BoardUtils.isValidTileCoordinate(candidateDestinationCoordinate)) {
                 continue;
@@ -33,7 +34,7 @@ public class Pawn extends Piece{
             if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()) {
                 // TO DO
                 legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate));
-            }
+            } 
         }
 
         return null;
