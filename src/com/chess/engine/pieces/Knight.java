206122b4755110d4b07139967a710212697c44f1
Wyatt92 <vi_hoang@hotmail.de>
2017-08-10 02:28:44 +0200

    #15 Board: CreateStandardBoard Method

--- src/com/chess/engine/pieces/Knight.java
+++ src/com/chess/engine/pieces/Knight.java
@@ -16,7 +16,7 @@ public class Knight extends Piece{
 
     private final static int[] CANDIDATE_MOVE_COORDINATES = { -17, -15, -10, -6, 6, 10, 15, 17};
 
-    Knight(int piecePosition, Alliance pieceAlliance) {
+    public Knight(final Alliance pieceAlliance, final int piecePosition) {
         super(piecePosition, pieceAlliance);
     }
 
