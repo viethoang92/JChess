9a6df25a6e9ea14fd928095d4bd21bffa643e34f
Wyatt92 <vi_hoang@hotmail.de>
2017-08-10 02:12:07 +0200

    #14 Board

--- src/com/chess/engine/board/Board.java
+++ src/com/chess/engine/board/Board.java
@@ -1,8 +1,61 @@
 package com.chess.engine.board;
 
+import com.chess.engine.Alliance;
+import com.chess.engine.pieces.Piece;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
 public class Board {
+
+    private final List<Tile> gameBoard;
+
+
+    // Constructor
+    private Board(Builder builder) {
+        this.gameBoard = createGameBoard(builder);
+    }
 
     public Tile getTile(final int tileCoordinate) {
         return null;
+    }
+
+    private static List<Tile> createGameBoard(final Builder builder) {
+        final Tile[] tiles = new Tile[BoardUtils.NUM_TILES];
+        for(int i=0; i< BoardUtils.NUM_TILES; i++) {
+            tiles[i] = Tile.createTile(i, builder.boardConfig.get(i));
+        }
+        return Collections.unmodifiableList(Arrays.asList(tiles));
+    }
+
+    public static Board createStandardBoard() {
+        return null;
+    }
+
+    public static class Builder {
+
+        Map<Integer, Piece> boardConfig;
+        Alliance nextMoveMaker;
+
+        public Builder() {
+
+        }
+
+        public Builder setPiece(final Piece piece) {
+            this.boardConfig.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public Builder setMoveMaker(final Alliance alliance) {
+            this.nextMoveMaker = nextMoveMaker;
+            return this;
+        }
+
+        public Board build() {
+            return new Board(this);
+        }
+
     }
 }
--- src/com/chess/engine/pieces/Piece.java
+++ src/com/chess/engine/pieces/Piece.java
@@ -9,10 +9,12 @@ import java.util.List;
 
 public abstract class Piece {
 
+    // Properties
     protected final int piecePosition;
     protected final Alliance pieceAlliance;
     protected final boolean isFirstMove;
 
+    // Constructor
     Piece(final int piecePosition, final Alliance pieceAlliance) {
         this.pieceAlliance = pieceAlliance;
         this.piecePosition = piecePosition;
@@ -20,6 +22,10 @@ public abstract class Piece {
         this.isFirstMove = false;
     }
 
+    // Getters
+    public int getPiecePosition() {
+        return this.piecePosition;
+    }
     public Alliance getPieceAlliance() {
         return this.pieceAlliance;
     }
